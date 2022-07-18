e64b192fa0b80f971d3d3b3076a765530354654b
Vineesh <vineeshm199812@gmail.com>
2022-07-18 23:14:50 +0530

    update

--- DJANGO__/countries/countriesproce.py
+++ DJANGO__/countries/countriesproce.py
@@ -25,13 +25,25 @@ print(ukraine_currency_data.pop().pop().get("name"))
 #borders of ukraine
 #print highest populated country
 
-india_languages=[country["languages"] for country in data if country["name"].lower()=="india"]
-print(india_languages.pop().pop())
+india_languages=[country["languages"] for country in data if country["name"].lower()=="india"].pop()
+print(india_languages)
 
-borders_ukraine=[country["borders"] for country in data if country["name"].lower()=="ukraine"]
-print(borders_ukraine.pop())
+language_name=[lan["name"] for lan in india_languages]
+print(language_name)
+#OR  ==> all in a single for loop
+language=[lan["name"]for country in data for lan in country["languages"] if country["name"].lower()=="india"]
+print(language)
 
-#print(max(data,key=lambda post:len(post["liked_by"])))
-population=max(data,key=lambda data:data["population"])
+borders_ukraine=[country["borders"] for country in data if country["name"].lower().startswith("ukraine")][0] #to avoid the list in o/p
+#print(borders_ukraine)
+borders=[country["name"] for country in data if country["alpha3Code"] in borders_ukraine]
+print(borders)
+
+population=max(data,key=lambda country:country["population"])
 print(population.get("name"))
 print(population.get("population"))
+
+#print countries whose languages is english
+
+english_countries=[country["name"] for country in data for lan in country["languages"] if lan["name"].lower()=="english"]
+print(english_countries)
